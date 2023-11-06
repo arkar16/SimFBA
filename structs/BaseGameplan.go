f670ab44d33ab21f6f5699a3083f8e44719601b8
CalebRose <ctrose17@gmail.com>
2023-11-06 07:14:58 -0800

    Updating new gameplan

--- structs/BaseGameplan.go
+++ structs/BaseGameplan.go
@@ -49,9 +49,8 @@ type BaseGameplan struct {
 	RunnerDistributionRB3        int
 	RunnerDistributionFB1        int
 	RunnerDistributionFB2        int
-	RunnerDistributionBK1        int
-	RunnerDistributionBK2        int
-	RunnerDistributionBK3        int
+	RunnerDistributionWR         int    // Jet Sweep stuff
+	RunnerDistributionWRPosition string // WR1, WR2, WR3, WR4, WR5
 	RunOutsideLeft               int
 	RunOutsideRight              int
 	RunInsideLeft                int
@@ -82,16 +81,27 @@ type BaseGameplan struct {
 	PeekInside                   int
 	PeekPower                    int
 	TargetingWR1                 int
+	TargetDepthWR1               string // Quick, Short, Long, None
 	TargetingWR2                 int
+	TargetDepthWR2               string
 	TargetingWR3                 int
+	TargetDepthWR3               string
 	TargetingWR4                 int
+	TargetDepthWR4               string
 	TargetingWR5                 int
+	TargetDepthWR5               string
 	TargetingTE1                 int
+	TargetDepthTE1               string
 	TargetingTE2                 int
+	TargetDepthTE2               string
 	TargetingTE3                 int
+	TargetDepthTE3               string
 	TargetingRB1                 int
+	TargetDepthRB1               string
 	TargetingRB2                 int
+	TargetDepthRB2               string
 	TargetingFB1                 int
+	TargetDepthFB1               string
 	DefensiveScheme              string
 	DefFormation1                string
 	DefFormation1RunToPass       int
@@ -125,6 +135,8 @@ type BaseGameplan struct {
 	HasSchemePenalty             bool
 	OffenseSchemePenalty         uint
 	DefenseSchemePenalty         uint
+	DefaultOffense               bool
+	DefaultDefense               bool
 }
 
 func (cg *BaseGameplan) ApplySchemePenalty(IsOffense bool) {
@@ -180,6 +192,8 @@ func (bg *BaseGameplan) UpdateCollegeGameplan(dto CollegeGameplan) {
 	bg.RunnerDistributionRB3 = dto.RunnerDistributionRB3
 	bg.RunnerDistributionFB1 = dto.RunnerDistributionFB1
 	bg.RunnerDistributionFB2 = dto.RunnerDistributionFB2
+	bg.RunnerDistributionWR = dto.RunnerDistributionWR
+	bg.RunnerDistributionWRPosition = dto.RunnerDistributionWRPosition
 	bg.RunOutsideLeft = dto.RunOutsideLeft
 	bg.RunOutsideRight = dto.RunOutsideRight
 	bg.RunInsideLeft = dto.RunInsideLeft
@@ -311,16 +325,27 @@ func (bg *BaseGameplan) UpdateNFLGameplan(dto NFLGameplan) {
 	bg.ChoiceInside = dto.ChoiceInside
 	bg.ChoicePower = dto.ChoicePower
 	bg.TargetingWR1 = dto.TargetingWR1
+	bg.TargetDepthWR1 = dto.TargetDepthWR1
 	bg.TargetingWR2 = dto.TargetingWR2
+	bg.TargetDepthWR2 = dto.TargetDepthWR2
 	bg.TargetingWR3 = dto.TargetingWR3
+	bg.TargetDepthWR3 = dto.TargetDepthWR3
 	bg.TargetingWR4 = dto.TargetingWR4
+	bg.TargetDepthWR4 = dto.TargetDepthWR4
 	bg.TargetingWR5 = dto.TargetingWR5
+	bg.TargetDepthWR5 = dto.TargetDepthWR5
 	bg.TargetingTE1 = dto.TargetingTE1
+	bg.TargetDepthTE1 = dto.TargetDepthTE1
 	bg.TargetingTE2 = dto.TargetingTE2
+	bg.TargetDepthTE2 = dto.TargetDepthTE2
 	bg.TargetingTE3 = dto.TargetingTE3
+	bg.TargetDepthTE3 = dto.TargetDepthTE3
 	bg.TargetingRB1 = dto.TargetingRB1
+	bg.TargetDepthRB1 = dto.TargetDepthRB1
 	bg.TargetingRB2 = dto.TargetingRB2
+	bg.TargetDepthRB2 = dto.TargetDepthRB2
 	bg.TargetingFB1 = dto.TargetingFB1
+	bg.TargetDepthFB1 = dto.TargetDepthFB1
 	bg.DefensiveScheme = dto.DefensiveScheme
 	bg.DefFormation1 = dto.DefFormation1
 	bg.DefFormation1RunToPass = dto.DefFormation1RunToPass
@@ -351,4 +376,6 @@ func (bg *BaseGameplan) UpdateNFLGameplan(dto NFLGameplan) {
 	bg.MaximumFGDistance = dto.MaximumFGDistance
 	bg.GoFor4AndLong = dto.GoFor4AndLong
 	bg.GoFor4AndShort = dto.GoFor4AndShort
+	bg.DefaultOffense = dto.DefaultOffense
+	bg.DefaultDefense = dto.DefaultDefense
 }

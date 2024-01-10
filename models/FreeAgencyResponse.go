d4eff47028f9ccee6d87d6656b06af49d8f9f6d4
CalebRose <ctrose17@gmail.com>
2024-01-10 06:16:05 -0800

    hotfix for FA page

--- models/FreeAgencyResponse.go
+++ models/FreeAgencyResponse.go
@@ -14,43 +14,47 @@ type FreeAgentResponse struct {
 	ID       uint
 	PlayerID int
 	structs.BasePlayer
-	TeamID          int
-	College         string
-	TeamAbbr        string
-	Hometown        string
-	State           string
-	Experience      uint
-	IsActive        bool
-	IsFreeAgent     bool
-	IsWaived        bool
-	MinimumValue    float64
-	PreviousTeam    string
-	DraftedTeam     string
-	ShowLetterGrade bool
-	IsPracticeSquad bool
-	Stats           []structs.NFLPlayerStats
-	SeasonStats     structs.NFLPlayerSeasonStats
-	Offers          []structs.FreeAgencyOffer
+	TeamID            int
+	College           string
+	TeamAbbr          string
+	Hometown          string
+	State             string
+	Experience        uint
+	IsActive          bool
+	IsFreeAgent       bool
+	IsWaived          bool
+	MinimumValue      float64
+	PreviousTeam      string
+	DraftedTeam       string
+	ShowLetterGrade   bool
+	IsPracticeSquad   bool
+	IsAcceptingOffers bool
+	IsNegotiating     bool
+	Stats             []structs.NFLPlayerStats
+	SeasonStats       structs.NFLPlayerSeasonStats
+	Offers            []structs.FreeAgencyOffer
 }
 
 type WaiverWirePlayerResponse struct {
 	ID       uint
 	PlayerID int
 	structs.BasePlayer
-	TeamID          int
-	College         string
-	TeamAbbr        string
-	Hometown        string
-	State           string
-	Experience      uint
-	IsActive        bool
-	IsFreeAgent     bool
-	IsWaived        bool
-	MinimumValue    float64
-	PreviousTeam    string
-	DraftedTeam     string
-	ShowLetterGrade bool
-	IsPracticeSquad bool
-	WaiverOffers    []structs.NFLWaiverOffer
-	Contract        structs.NFLContract
+	TeamID            int
+	College           string
+	TeamAbbr          string
+	Hometown          string
+	State             string
+	Experience        uint
+	IsActive          bool
+	IsFreeAgent       bool
+	IsWaived          bool
+	IsAcceptingOffers bool
+	IsNegotiating     bool
+	MinimumValue      float64
+	PreviousTeam      string
+	DraftedTeam       string
+	ShowLetterGrade   bool
+	IsPracticeSquad   bool
+	WaiverOffers      []structs.NFLWaiverOffer
+	Contract          structs.NFLContract
 }

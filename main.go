f649b93b2d59a5ade8164f8948792e1f04f85e9b
CalebRose <ctrose17@gmail.com>
2024-04-06 06:53:29 -0700

    adjusting the timeslot slightly

--- main.go
+++ main.go
@@ -309,16 +309,16 @@ func handleCron() {
 		c.AddFunc("0 21 * * 4", controller.ShowCFBThursdayViaCron) // Thurs Night
 		c.AddFunc("0 20 * * 4", controller.ShowNFLThursdayViaCron) // Thurs NFL
 		c.AddFunc("0 21 * * 5", controller.ShowCFBFridayViaCron)   // Fri Night
-		c.AddFunc("0 14 * * 6", controller.ShowCFBSatMornViaCron)  // Sat. Morning
-		c.AddFunc("0 16 * * 6", controller.ShowCFBSatAftViaCron)   // Sat. Afternoon
-		c.AddFunc("0 18 * * 6", controller.ShowCFBSatEveViaCron)   // Sat. Evening
+		c.AddFunc("0 15 * * 6", controller.ShowCFBSatMornViaCron)  // Sat. Morning
+		c.AddFunc("0 17 * * 6", controller.ShowCFBSatAftViaCron)   // Sat. Afternoon
+		c.AddFunc("0 19 * * 6", controller.ShowCFBSatEveViaCron)   // Sat. Evening
 		c.AddFunc("0 21 * * 6", controller.ShowCFBSatNitViaCron)   // Sat. Night
-		c.AddFunc("0 14 * * 0", controller.ShowNFLSunNoonViaCron)  // Sun Noon
-		c.AddFunc("0 16 * * 0", controller.ShowNFLSunAftViaCron)   // Sun Aft
-		c.AddFunc("0 18 * * 0", controller.ShowNFLSunNitViaCron)   // Sun Nit
-		c.AddFunc("0 16 * * 1", controller.ShowNFLMonNitViaCron)   // Mon Nit
+		c.AddFunc("0 15 * * 0", controller.ShowNFLSunNoonViaCron)  // Sun Noon
+		c.AddFunc("0 17 * * 0", controller.ShowNFLSunAftViaCron)   // Sun Aft
+		c.AddFunc("0 19 * * 0", controller.ShowNFLSunNitViaCron)   // Sun Nit
+		c.AddFunc("0 17 * * 1", controller.ShowNFLMonNitViaCron)   // Mon Nit
 		// // Sync Week
-		c.AddFunc("0 17 * * 1", controller.SyncToNextWeekViaCron)
+		c.AddFunc("0 18 * * 1", controller.SyncToNextWeekViaCron)
 		c.Start()
 	}()
 }
